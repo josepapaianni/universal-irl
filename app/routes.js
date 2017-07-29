dc59136c589463aca7632decb16cb0dcb88da78b
josepapaianni <jose.papaianni@mercadolibre.com>
2017-07-28 21:42:47 -0300

    refactor structure

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,7 @@
 if (typeof require.ensure !== 'function') require.ensure = (d, c) => c(require);
 
 const React = require('react');
-const AsyncRoute = require('./helpers/async-route');
+const AsyncRoute = require('./components/async-route');
 
 let Home, About, Contact, Me;
 
@@ -10,19 +10,19 @@ if (process.env.BROWSER) {
   // because you need to resolve the dependency in a synchronous way to match
   // the app rendered in the server with the app rendered in the client.
   // You return a component instead of a promise if the chunk is already loaded
-  Home = () => __webpack_modules__[require.resolveWeak('./Home')] ? __webpack_require__(require.resolveWeak('./Home')) :
-    new Promise(resolve => require.ensure([], require => resolve(require('./Home')), null, 'home'));
-  About = () => __webpack_modules__[require.resolveWeak('./About')] ? __webpack_require__(require.resolveWeak('./About')) :
-    new Promise(resolve => require.ensure([], require => resolve(require('./About')), null, 'about'));
-  Contact = () => __webpack_modules__[require.resolveWeak('./Contact')] ? __webpack_require__(require.resolveWeak('./Contact')) :
-    new Promise(resolve => require.ensure([], require => resolve(require('./Contact')), null, 'contact'));
-  Me = () => __webpack_modules__[require.resolveWeak('./Me')] ? __webpack_require__(require.resolveWeak('./Me')) :
-    new Promise(resolve => require.ensure([], require => resolve(require('./Me')), null, 'me'));
+  Home = () => __webpack_modules__[require.resolveWeak('./home')] ? __webpack_require__(require.resolveWeak('./home')) :
+    new Promise(resolve => require.ensure([], require => resolve(require('./home')), null, 'home'));
+  About = () => __webpack_modules__[require.resolveWeak('./about')] ? __webpack_require__(require.resolveWeak('./about')) :
+    new Promise(resolve => require.ensure([], require => resolve(require('./about')), null, 'about'));
+  Contact = () => __webpack_modules__[require.resolveWeak('./contact')] ? __webpack_require__(require.resolveWeak('./contact')) :
+    new Promise(resolve => require.ensure([], require => resolve(require('./contact')), null, 'contact'));
+  Me = () => __webpack_modules__[require.resolveWeak('./about/me')] ? __webpack_require__(require.resolveWeak('./about/me')) :
+    new Promise(resolve => require.ensure([], require => resolve(require('./about/me')), null, 'me'));
 } else {
-  Home = () => require('./Home');
-  About = () => require('./About');
-  Contact = () => require('./Contact');
-  Me = () => require('./Me');
+  Home = () => require('./home');
+  About = () => require('./about');
+  Contact = () => require('./contact');
+  Me = () => require('./about/me');
 }
 
 
